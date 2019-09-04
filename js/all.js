31ca792ef3cc593fab4975d2b6972650b93b8a0e
yangyang1228 <1308128753@qq.com>
2019-09-04 13:04:39 +0800

    09040104

---
+++ js/all.js
@@ -0,0 +1,44 @@
+window.onload=function(){
+	var opgup=document.getElementById('pgup');
+	var opgdown=document.getElementById('pgdown');
+	var oimg=document.getElementsByClassName('image');
+	var oimglink=document.getElementsByClassName('image-link');
+	var pagenum=1;
+	opgdown.onclick=function(){
+		if(pagenum<7){
+			pagenum++;
+			var imgsrc=(pagenum-1)*9;
+			for(i=1; i<=9 ; i++){
+				imgsrc++;
+				oimg[i-1].getAttributeNode('src').value="img/"+imgsrc+".jpg";
+				oimglink[i-1].getAttributeNode('href').value="img/"+imgsrc+".jpg";
+			}
+			if(pagenum==7){
+				oimglink[8].style.display="none";
+				oimglink[7].style.display="none";
+				oimglink[6].style.display="none";
+			}		
+			
+			
+		}
+			
+
+	}
+	
+	opgup.onclick=function(){		
+		if(pagenum>1){
+			oimglink[8].style.display="inline-block";
+			oimglink[7].style.display="inline-block";
+			oimglink[6].style.display="inline-block";
+			pagenum--;
+			var imgsrc=(pagenum-1)*9;
+			for(i=1; i<=9 ; i++){
+				imgsrc++;
+				oimg[i-1].getAttributeNode('src').value="img/"+imgsrc+".jpg";
+				oimglink[i-1].getAttributeNode('href').value="img/"+imgsrc+".jpg";
+			}		
+		}
+	
+	}
+	
+}
